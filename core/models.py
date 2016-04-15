f1e33290e27c26849d78e79a0dd2e0a8f0aa82fd
Luis Garcia <luisgarciaestrades@gmail.com>
2016-04-15 18:54:15 +0200

    Added textblocks

--- core/models.py
+++ core/models.py
@@ -2,4 +2,11 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    name=models.CharField(max_length=30)
+    email=models.EmailField(max_length=254)
+    subject=models.CharField(max_length=60)
+    message=models.TextField()
+
+    def __unicode__(self):
+        return self.subject + ': ' + self.email
--- core/views.py
+++ core/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.views import generic
+from django.views.generic import CreateView
+from .models import Contact
+from textblock.models import TextBlock
+
+class IndexView(CreateView):
+    model = Contact
+    fields = ['name', 'email', 'subject', 'message']
+    template_name = 'index.html'
+    success_url = '/'
 
 
-def index(request):
-    context = {}
-    return render(request, 'index.html', context)
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        textblocks = {}
+        for block in TextBlock.objects.all():
+            textblocks[block.slug] = block
+        context['textblocks'] = textblocks
+        return context
