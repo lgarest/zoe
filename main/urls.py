0254aaca771ca8cde04a40665562f9af14ad33c9
Luis Garcia <luisgarciaestrades@gmail.com>
2016-04-24 21:24:01 +0200

    updated with second design

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-from views import IndexView
+from views import IndexView, ZOEhubView, ZOEaiView, UsesView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view()),
+    url(r'^$', IndexView.as_view(), name='home'),
+    url(r'^zoehub$', ZOEhubView.as_view(), name='zoehub'),
+    url(r'^zoeai$', ZOEaiView.as_view(), name='zoeai'),
+    url(r'^uses$', UsesView.as_view(), name='uses'),
+    url(r'^buy$', IndexView.as_view(), name='buy')
 ]
--- main/views.py
+++ main/views.py
@@ -1,16 +1,54 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
 from .models import Contact
 from textblock.models import TextBlock
 
 class IndexView(CreateView):
     model = Contact
     fields = ['name', 'email', 'subject', 'message']
-    template_name = 'index.html'
+    template_name = 'home.html'
     success_url = '/'
 
 
     def get_context_data(self, **kwargs):
         context = super(IndexView, self).get_context_data(**kwargs)
+        textblocks = {}
+        for block in TextBlock.objects.all():
+            textblocks[block.slug] = block
+        context['textblocks'] = textblocks
+        return context
+
+class ZOEhubView(TemplateView):
+
+    template_name = "zoehub.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ZOEhubView, self).get_context_data(**kwargs)
+        textblocks = {}
+        for block in TextBlock.objects.all():
+            textblocks[block.slug] = block
+        context['textblocks'] = textblocks
+        return context
+
+
+class ZOEaiView(TemplateView):
+
+    template_name = "zoeai.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(ZOEaiView, self).get_context_data(**kwargs)
+        textblocks = {}
+        for block in TextBlock.objects.all():
+            textblocks[block.slug] = block
+        context['textblocks'] = textblocks
+        return context
+
+
+class UsesView(TemplateView):
+
+    template_name = "uses.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(UsesView, self).get_context_data(**kwargs)
         textblocks = {}
         for block in TextBlock.objects.all():
             textblocks[block.slug] = block
